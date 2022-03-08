b2896b5a56c1cf3727bac8e64f8ee87cb8c0b790
aneerati <78110893+aneerati@users.noreply.github.com>
2022-03-08 09:59:55 -0600

    Update Six.js

--- src/pages/Six.js
+++ src/pages/Six.js
@@ -1,1 +1,33 @@
-h
+import React from 'react'
+import Template from "./Template"
+
+const Six = () => {
+  return (
+    <>
+        <Template
+        name={"Six Flags: Great Adventure"}
+        place_num={2}
+        stars={5}
+        mapLink={"https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d24401.65835769652!2d-74.45767035048435!3d40.13766599691901!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c1649b50339cd7%3A0xeed622849d816ad4!2sSix%20Flags%20Great%20Adventure-%20Theme%20Park%2C%20Jackson%20Township%2C%20NJ%2008514!5e0!3m2!1sen!2sus!4v1646341214732!5m2!1sen!2sus"}
+        pnumb={"(123) 456-7890"}
+        address={"1 Six Flags Blvd, Jackson Township, NJ 08527"}
+        hours={"Sun	10AM–5PM Mon 10AM–5PM Tues 10AM–5PM Wed 10AM–5PM Thurs 10AM–5PM Fri 10AM–12PM Sat 10AM–5PM"}
+        website={"https://www.sixflags.com/greatadventure"}
+        faq1q={"FAQ #1"}
+        faq1a={"FAQ #1 answer"}
+        faq2q={"FAQ #2"}
+        faq2a={"FAQ #2 answer"}
+        faq3q={"FAQ #3"}
+        faq3a={"FAQ #3 answer"}
+        h1={"Highlight 1"}
+        h2={"Highlight 2"}
+        h3={"Highlight 3"}
+        about={"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."}
+        thingstoknow={"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."}
+        covid={"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."}
+        addInfo={"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."} />
+    </>
+  )
+}
+
+export default Six
