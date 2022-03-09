4402fa0a7feac1a750466db9853dec9562be4036
aneerati <78110893+aneerati@users.noreply.github.com>
2022-03-09 10:45:13 -0500

    Create Princeton.js

---
+++ src/pages/Princeton.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import Template from "./Template"
+
+const Princeton = () => {
+  return (
+    <>
+        <Template
+        name={"Princeton"}
+        place_num={10}
+        stars={5}
+        mapLink={"https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d194606.2706566907!2d-74.809584698004!3d40.348490886151886!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c3e6c172d059f1%3A0x3fcd49934a13d6c2!2sPrinceton%2C%20NJ!5e0!3m2!1sen!2sus!4v1646341505314!5m2!1sen!2sus"}
+        pnumb={"609-924-4141"}
+        address={"400 Witherspoon Street, Princeton, NJ, 08540, US"}
+        hours={"Sun	11AM–9PM Mon 11AM–9PM Tues 11AM–9PM Wed 11AM–9PM Thurs 11AM–9PM Fri 11AM–10PM Sat 11AM–10PM"}
+        website={"https://www.six flags.com/greatadventure"}
+        faq1q={"FAQ #1"}
+        faq1a={"FAQ #1 answer"}
+        faq2q={"FAQ #2"}
+        faq2a={"FAQ #2 answer"}
+        faq3q={"FAQ #3"}
+        faq3a={"FAQ #3 answer"}
+        h1={"Highlight 1"}
+        h2={"Highlight 2"}
+        h3={"Highlight 3"}
+        about={"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."}
+        thingstoknow={"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."}
+        covid={"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."}
+        addInfo={"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."} />
+    </>
+  )
+}
+
+export default Princeton
