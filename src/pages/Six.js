ca3691a431aa1d369bf981dd0f96b6bed47fa852
aneerati <78110893+aneerati@users.noreply.github.com>
2022-03-14 08:26:57 -0400

    Update Six.js

--- src/pages/Six.js
+++ src/pages/Six.js
@@ -5,7 +5,7 @@ const Six = () => {
   return (
     <>
         <Template
-        name={"Six Flags: Great Adventure"}
+        name={"Six Flags Great Adventure"}
         place_num={2}
         stars={5}
         mapLink={"https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d24401.65835769652!2d-74.45767035048435!3d40.13766599691901!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c1649b50339cd7%3A0xeed622849d816ad4!2sSix%20Flags%20Great%20Adventure-%20Theme%20Park%2C%20Jackson%20Township%2C%20NJ%2008514!5e0!3m2!1sen!2sus!4v1646341214732!5m2!1sen!2sus"}
@@ -20,11 +20,11 @@ const Six = () => {
         faq3q={"Where can I get food?"}
         faq3a={"There are several food options inside the park, such as Johnny Rockets and Panda Express."}
         h1={"Kingda Ka"}
-        h2={"Hurrican Harbor"}
+        h2={"can Harbor"}
         h3={"Wild Safari Drive-Thru Adventure"}
-        about={"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."}
-        thingstoknow={"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."}
-        covid={"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."}
+        about={"Six Flags Great Adventure is an amusement park located in Jackson, New Jersey. The park features over a dozen major roller coaster and 52 other main attractions. Great Adventure is known for its thrilling coasters, such as Nitro, The Joker, and the tallest roller coaster in the world, Kingda Ka. In the summer, Hurrican Harbor, Six Flag's water park, opens for those looking for a splash. The water park features a large wave pool, a lazy river, and many attractions for thrill seekers ready to get wet. For a calmer and more family experience, the Wild Safari Drive-Thru Adventure is the perfect attraction for young kids. The safari allows parkgoers to view and sometimes interact will large animals like giraffes and rinhos without confining them in a small space. Both parks offer a variety of food options to ensure no family goes hungry."}
+        thingstoknow={"Pets are not allowed inside any of the parks, with the exception of service animals. Almost all major attractions in Great Adventure and Hurrican Harbor have a height restriction, so being too short or too tall may affect your riding status. Unless you are on a meal plan with the park, the food options are not included, so it is important to carry money. Since many major attractions are high-speed roller coasters or water rides, it is recommended that parkgoers do not carry many belongings with them or they should rent a locker to store their items."}
+        covid={"Face coverings at Six Flags attractions are not required but highly recommended for unvaccinated guests. There is also no vaccine requirement. Six Flags is committed to providing a healthy visit by implementing many hand sanitizing stations around the park."}
         addInfo={"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."} />
     </>
   )
